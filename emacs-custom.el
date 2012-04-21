6992607f7c1563703504ddce6122c98f5384c87c
suijinxin@school <suijinxin@gmail.com>
2012-04-21 16:52:03 +0800

    @school

--- emacs-custom.el
+++ emacs-custom.el
@@ -6,15 +6,15 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- ;'(muse-colors-autogen-headings (quote outline))
- ;'(muse-colors-inline-image-method (quote muse-colors-use-publishing-directory))
- ;'(muse-completing-read-function (quote ido-completing-read)))
+ '(muse-colors-autogen-headings (quote outline))
+ '(muse-colors-inline-image-method (quote muse-colors-use-publishing-directory))
+ '(muse-completing-read-function (quote ido-completing-read)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-'(font-lock-builtin-face ((t (:foreground "LightCoral" :height 1.2))))
+ '(font-lock-builtin-face ((t (:foreground "LightCoral" :height 1.2))))
  '(font-lock-constant-face ((t (:foreground "gold" :height 1.2))))
  '(font-lock-doc-face ((t (:foreground "BlanchedAlmond" :weight bold :height 1.2))))
  '(font-lock-function-name-face ((t (:foreground "goldenrod" :weight bold :height 1.2))))
